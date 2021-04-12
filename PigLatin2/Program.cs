9e1777dd3110acd74ee96003fcc8bab8027d9d6c
Jason <j_schubb53@yahoo.com>
2021-04-12 08:43:16 -0400

    first commit

---
+++ PigLatin2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PigLatin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine("Enter a word to be translated");
+            string input = Console.ReadLine();
+            string x = input.ToLower();
+            string pig;
+            const string vowels = "aeiou";
+            string a;
+            string b;
+            int pos;
+            {
+                foreach (string word in x.Split())
+                {
+                    a = word.Substring(0, 1);
+                    b = word.Substring(1, word.Length, -1);
+                    pos = vowels.IndexOf(a);
+                    if (pos == -1)
+                    {
+                        pig = b + a + "ay";
+                    }
+                    else
+                    {
+                        pig = word + "way";
+                    }
+                    
+                }
+                return (pig);
+            }
+
+        }
+    }
+}
+
+
